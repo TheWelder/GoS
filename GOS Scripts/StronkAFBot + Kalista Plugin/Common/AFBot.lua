cbdf8fa0ee72a6b655814c88589ec7171348e8be
TheWelder <mikael.j.santos@gmail.com>
2015-07-21 23:03:05 +0100

    minor changes

--- GOS Scripts/StronkAFBot + Kalista Plugin/Common/AFBot.lua
+++ GOS Scripts/StronkAFBot + Kalista Plugin/Common/AFBot.lua
@@ -1,6 +1,6 @@
 local myHero = nil
  
-function ObjectLoopEvent(Object,myHero)
+AFBot(function(myHero
         --DrawText(string.format("%s by TheWelder Loaded ", GetObjectName(myHero)),24,750,50,0xff00ff00);
         local Obj_Type = GetObjectType(Object);
         if Obj_Type == Obj_AI_Hero then
@@ -9,4 +9,4 @@ function ObjectLoopEvent(Object,myHero)
                         MoveToXYZ(hero_origin.x-100,hero_origin.y-100,hero_origin.z-100)
                 end
         end
-end
+end)
