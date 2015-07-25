46522dab82fc7484e4d4312b9f400b81e16ffcc9
TheWelder <mikael.j.santos@gmail.com>
2015-07-25 20:16:17 +0100

    ver 1.0.0.2

--- GOS Scripts/LeeSin/testscript1.lua
+++ GOS Scripts/LeeSin/testscript1.lua
@@ -1,9 +1,15 @@
 require('Inspired')
 require('twgank')
 
---ver 1.0.0.1, added GetCloserIsnecToMinion
+--ver 1.0.0.2, added GetCloserIsnecToMinion
 --api 0.0.4
 --Made by TheWelder
+
+--ItemGhostWard aka sightstone 2049
+--sightward aka ward 2044
+
+
+items = {}
 
 local myHero = nil
 local champName = nil
@@ -14,16 +20,12 @@ AddButton("Q", "Use Q", true)
 AddButton("W", "Use W", true)
 AddButton("E", "Use E", true)
 AddButton("R", "Use R", true)
-AddInfo("util", "Utilities")
-AddButton("Gank","GankAlert",true)
 
 OnLoop(function(myHero)
 myHero = GetMyHero()
 myHeroPos = GetOrigin(GetMyHero())
 champName = GetObjectName(myHero)
 target = GetCurrentTarget()
-local targetPos = GetOrigin(target)
-local Obj_Type = GetObjectType(target)
 
 local allDMG = GetBonusDmg(myHero)+GetBaseDamage(myHero)
 local perc90 = (allDMG*90)/100
@@ -31,9 +33,19 @@ local Qdmg = (GetCastLevel(myHero,_Q)*30)+50+perc90
 local Edmg =(GetCastLevel(myHero,_E)*35)+25+allDMG
 local Rdmg = (GetCastLevel(myHero,_R)*200)+(allDMG*2)
 
+local minion = ClosestMinion(myHeroPos,MINION_ALLY)
+local minionPos = GetOrigin(minion)
+local warden = GetWards(myTeam)
+local wardenPos = GetOrigin(warden)
+local hero_origin = myHeroPos
+local targetPos = GetOrigin(target)
+local myscreenpos = WorldToScreen(1,hero_origin.x,hero_origin.y,hero_origin.z)
+
+local ward1 = GetItemSlot(myHero,2045),GetItemSlot(myHero,2044),GetItemSlot(myHero,2049),GetItemSlot(myHero,2043)
+
 	if champName == "LeeSin" then
-		DrawText(string.format("%s LE STAR", GetObjectName(myHero)),24,750,50,0xff00ff00);
-		if KeyIsDown(0x20) then
+		DrawText(string.format("%s LeStar", GetObjectName(myHero)),24,750,50,0xff00ff00);
+		if KeyIsDown(0x20) and IsObjectAlive(target) then
 			if GetButtonValue("Q") then
 				if ValidTarget(target,1000) then
 					local FQpred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2000,250,900,50,true,true)
@@ -43,24 +55,33 @@ local Rdmg = (GetCastLevel(myHero,_R)*200)+(allDMG*2)
 						CastTargetSpell(target,GetItemSlot(myHero,3144))
 					end
 				end
-			end
-			if GetButtonValue("W") then
-			local minion = ClosestMinion(myHeroPos,MINION_ALLY)
-				if minion and GetOrigin(minion) then
-					if GetDistance(minion, myHero) <= 700 and CanUseSpell(myHero,_W) == READY then       
-						if GetDistance(GetOrigin(minion),GetOrigin(target)) < 450 then
-							CastTargetSpell(minion,_W)
-						end
-					end	
-				end
-			end				
+			end		
 			if GetButtonValue("E") then
 				if ValidTarget(target,350) then
 					local FQpred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2000,250,350,50,true,true)
 					if CanUseSpell(myHero,_E) == READY then
 						CastSkillShot(_E,FQpred.PredPos.x,FQpred.PredPos.y,FQpred.PredPos.z)
-						CastSkillShot(GetItemSlot(myHero,3077),FQpred.PredPos.x,FQpred.PredPos.y,FQpred.PredPos.z)
-						CastSkillShot(GetItemSlot(myHero,3074),FQpred.PredPos.x,FQpred.PredPos.y,FQpred.PredPos.z)
+						CastTargetSpell(target,GetItemSlot(myHero,3077))
+						CastTargetSpell(target,GetItemSlot(myHero,3074))
+						CastTargetSpell(target,GetItemSlot(myHero,3143))
+
+					end
+				end
+			end
+			if GetButtonValue("W") then
+				if IsInDistance(target,1300) and IsObjectAlive(target) then
+					if GetDistance(minion,target) < GetDistance(myHero,target) and GetDistance(myHero,target) > 200 then 
+						if minion and GetOrigin(minion) then
+							if CanUseSpell(myHero,_W) == READY then
+								CastTargetSpell(minion,_W)	
+							end
+						end
+					end
+					else
+					if GetDistance(myHero,target) < 200 then 
+						if CanUseSpell(myHero,_W) == READY then
+							CastTargetSpell(myHero,_W)	
+						end
 					end
 				end
 			end
@@ -74,7 +95,17 @@ local Rdmg = (GetCastLevel(myHero,_R)*200)+(allDMG*2)
 				end
 			end
 		end
-		else
-		DrawText(string.format("%s not suported", GetObjectName(myHero)),24,750,50,0xffffff00)	
+	else
+	DrawText(string.format("%s not suported", GetObjectName(myHero)),24,750,50,0xffffff00)
 	end
+end)
+
+OnProcessSpell(function(target,spell)
+
+	if target and target == myHero and spell then
+		if spell.name:lower():find("itemghostward") then
+			waitTickCount = GetTickCount() + 13500
+		end
+	end
+
 end)
