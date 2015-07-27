7f4c5b5f5324bc386ed23f08a0fb2f80f6e69ddc
TheWelder <mikael.j.santos@gmail.com>
2015-07-27 16:36:54 +0100

    Fixed

--- GOS Scripts/LeeSin/testscript1.lua
+++ GOS Scripts/LeeSin/testscript1.lua
@@ -1,6 +1,6 @@
 require('Inspired')
-require('twgank')
 
+wardTable = {}
 --ver 1.0.0.2, added GetCloserIsnecToMinion
 --api 0.0.4
 --Made by TheWelder
@@ -8,24 +8,25 @@ require('twgank')
 --ItemGhostWard aka sightstone 2049
 --sightward aka ward 2044
 
-
-items = {}
-
-local myHero = nil
-local champName = nil
-local target = nil
-
-AddInfo("leesin", "LeStar:")
-AddButton("Q", "Use Q", true)
-AddButton("W", "Use W", true)
-AddButton("E", "Use E", true)
-AddButton("R", "Use R", true)
+Config = scriptConfig("leesin", "LeStar:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("Combo", "Combo", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
 
 OnLoop(function(myHero)
-myHero = GetMyHero()
-myHeroPos = GetOrigin(GetMyHero())
-champName = GetObjectName(myHero)
-target = GetCurrentTarget()
+
+local myHero = GetMyHero()
+local myHeroPos = GetOrigin(myHero)
+local champName = GetObjectName(myHero)
+local target = GetCurrentTarget()
+local targetPos = GetOrigin(target)
+
+local minion = ClosestMinion(myHeroPos,MINION_ALLY)
+local minionPos = GetOrigin(minion)
+local hero_origin = myHeroPos
+local myscreenpos = WorldToScreen(1,hero_origin.x,hero_origin.y,hero_origin.z)
 
 local allDMG = GetBonusDmg(myHero)+GetBaseDamage(myHero)
 local perc90 = (allDMG*90)/100
@@ -33,20 +34,15 @@ local Qdmg = (GetCastLevel(myHero,_Q)*30)+50+perc90
 local Edmg =(GetCastLevel(myHero,_E)*35)+25+allDMG
 local Rdmg = (GetCastLevel(myHero,_R)*200)+(allDMG*2)
 
-local minion = ClosestMinion(myHeroPos,MINION_ALLY)
-local minionPos = GetOrigin(minion)
-local warden = GetWards(myTeam)
-local wardenPos = GetOrigin(warden)
-local hero_origin = myHeroPos
-local targetPos = GetOrigin(target)
-local myscreenpos = WorldToScreen(1,hero_origin.x,hero_origin.y,hero_origin.z)
 
-local ward1 = GetItemSlot(myHero,2045),GetItemSlot(myHero,2044),GetItemSlot(myHero,2049),GetItemSlot(myHero,2043)
+
+--local ward1 = GetItemSlot(myHero,2045),GetItemSlot(myHero,2044),GetItemSlot(myHero,2049),GetItemSlot(myHero,2043)
 
 	if champName == "LeeSin" then
 		DrawText(string.format("%s LeStar", GetObjectName(myHero)),24,750,50,0xff00ff00);
-		if KeyIsDown(0x20) and IsObjectAlive(target) then
-			if GetButtonValue("Q") then
+		--if KeyIsDown(0x20) and IsObjectAlive(target) then
+		if Config.Combo then
+			if Config.Q then
 				if ValidTarget(target,1000) then
 					local FQpred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2000,250,900,50,true,true)
 					if CanUseSpell(myHero,_Q) == READY and FQpred.HitChance == 1 then
@@ -56,7 +52,7 @@ local ward1 = GetItemSlot(myHero,2045),GetItemSlot(myHero,2044),GetItemSlot(myHe
 					end
 				end
 			end		
-			if GetButtonValue("E") then
+			if Config.E then
 				if ValidTarget(target,350) then
 					local FQpred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2000,250,350,50,true,true)
 					if CanUseSpell(myHero,_E) == READY then
@@ -68,7 +64,7 @@ local ward1 = GetItemSlot(myHero,2045),GetItemSlot(myHero,2044),GetItemSlot(myHe
 					end
 				end
 			end
-			if GetButtonValue("W") then
+			if Config.W then
 				if IsInDistance(target,1300) and IsObjectAlive(target) then
 					if GetDistance(minion,target) < GetDistance(myHero,target) and GetDistance(myHero,target) > 200 then 
 						if minion and GetOrigin(minion) then
@@ -85,7 +81,7 @@ local ward1 = GetItemSlot(myHero,2045),GetItemSlot(myHero,2044),GetItemSlot(myHe
 					end
 				end
 			end
-			if GetButtonValue("R") then
+			if Config.R then
 				if ValidTarget(target,375) then
 					if CalcDamage(myHero, target, Rdmg) > GetCurrentHP(target) + GetHPRegen(target) then
 						if CanUseSpell(myHero,_R) == READY then
@@ -109,3 +105,37 @@ OnProcessSpell(function(target,spell)
 	end
 
 end)
+
+--Credits to Inspired
+
+function ClosestMinion(pos, team)
+    local minion = nil
+    for k,v in pairs(GetAllMinions()) do 
+        local objTeam = GetTeam(v)
+        if not minion and v and objTeam == team then minion = v end
+        if minion and v and objTeam == team and GetDistanceSqr(GetOrigin(minion),pos) > GetDistanceSqr(GetOrigin(v),pos) then
+            minion = v
+        end
+    end
+    return minion
+end
+
+function GetDistanceSqr(p1,p2)
+    p2 = p2 or GetMyHeroPos()
+    local dx = p1.x - p2.x
+    local dz = (p1.z or p1.y) - (p2.z or p2.y)
+    return dx*dx + dz*dz
+end
+
+function ClosestAlly(pos, team)
+    local minion = nil
+    for k,v in pairs(GetAllMinions()) do 
+        local objTeam = GetTeam(v)
+        if not minion and v and objTeam == team then minion = v end
+        if minion and v and objTeam == team and GetDistanceSqr(GetOrigin(minion),pos) > GetDistanceSqr(GetOrigin(v),pos) then
+            minion = v
+        end
+    end
+    return minion
+end
+
