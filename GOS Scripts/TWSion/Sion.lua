0821586d7967a91ea903b78e0fafb3e157dc7780
TheWelder <mikael.j.santos@gmail.com>
2015-08-09 15:37:15 +0100

    added

---
+++ GOS Scripts/TWSion/Sion.lua
@@ -0,0 +1,44 @@
+require('Inspired')
+require('IAC')
+require('twgank')
+myIAC = IAC()
+Config = scriptConfig("sion", "TWSion")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("Combo", "Combo", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
+
+OnLoop(function(myHero)
+local myHero = GetMyHero()
+local myHeroPos = GetOrigin(myHero)
+local QRange = GetCastRange(myHero,_Q)
+local WRange = GetCastRange(myHero,_W)
+local ERange = GetCastRange(myHero,_E)
+local target = GetCurrentTarget()
+local myAttackRange = GetRange(myHero)
+local tarAttackRange = GetRange(target)
+	if "Sion" == GetObjectName(myHero) then
+		if Config.Q and ValidTarget(target,600) and IsInDistance(myHero,600) then
+			local QPred = GetPredictionForPlayer(myHeroPos, target,GetMoveSpeed(target),1400,250,600,55,true,true)
+			if CanUseSpell(myHero,_Q) == READY then
+				CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+				AttackUnit(target)
+			end
+		end
+		if Config.W and ValidTarget(target,WRange) and IsInDistance(myHero,WRange) then
+			if CanUseSpell(myHero,_W) == READY then
+				CastSpell(_W)
+				AttackUnit(target)
+			end
+		end
+		if Config.E and ValidTarget(target,ERange) and IsInDistance(myHero,ERange) then
+			local QPred = GetPredictionForPlayer(myHeroPos, target,GetMoveSpeed(target),1400,250,ERange,55,true,true)
+			if CanUseSpell(myHero,_E) == READY then
+				CastSkillShot(_E,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+				AttackUnit(target)
+			end
+		end
+	end
+
+end)
